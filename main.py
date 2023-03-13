38b117b749ccd140ab143ce0e0fff0fecdf946b3
gabriel <nune.gabriel.gn@gmail.com>
2023-03-13 12:00:50 +0000

    ADD CHANGES

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
+
+@app.get('/greet/{name}')
+def great_name(name:str):
+    return {"greetings":f"Hello {name} "}
